16059c1924ce8745a85bcce20520ac46f9b71a31
wang704 <78860015+wang704@users.noreply.github.com>
2024-11-02 12:26:45 +0800

    update

---
+++ draw.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(0,2*np.pi,100)
+x = 16*np.sin(t)**3
+y=13*np.cos(t) - 5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)
+
+plt.figure(figsize=(6,6))
+plt.plot(x,y,color='red')
+plt.title('draw by wxy')
+plt.axis('equal')
+plt.show()
